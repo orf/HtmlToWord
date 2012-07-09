800b608428535996f7331d503f3af5c9e12fed3d
Orf <tom@tomforb.es>
2012-07-09 16:10:10 +0100

    Added REQUIRES and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HtmlToWord',
+    version='0.1',
+    packages=['HtmlToWord', 'HtmlToWord.elements'],
+    url='https://github.com/orf/HtmlToWord',
+    license='',
+    author='Tom',
+    author_email='tom@tomforb.es',
+    description='Render HTML to a specific portion of a word document'
+)
