118bd463d0f0f0e9589de7a64b0c077b87c4d8d3
Orf <tom@tomforb.es>
2012-07-10 13:40:50 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HtmlToWord',
-    version='0.1.2',
+    version='0.1.3',
     packages=['HtmlToWord', 'HtmlToWord.elements'],
     url='https://github.com/orf/HtmlToWord',
     license='',
