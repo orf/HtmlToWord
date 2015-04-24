8c001a9c977892e20cbca2be817b4b2d1878ed70
luca bocchi <luca.bocchi@contextis.co.uk>
2015-04-24 16:37:19 +0100

    Calculate the maximum table size using the mapper for better handling of rowspans

--- HtmlToWord/elements/Table.py
+++ HtmlToWord/elements/Table.py
@@ -6,16 +6,12 @@ from win32com.client import constants
 
 class Table(BaseElement):
     """
-    Ding dong i'm a f*****g table.
-    I'm not a very good table, I can't have merged cells, and this makes me sad.
-    I support two types of table:
+    I seem to support merged cells and rows now, but in their infinite complexity I am
+    sure someone will find ways to break me. I support two types of table:
      * Simple ones with no tbody or thead (just <table><tr><td>...)
      * Complex ones with a tbody and thead element.
     """
     AllowedChildren = ["TableRow", "TableBody", "TableHead"]
-    CellsToMerge = []
-    rows = 0
-    columns = 0
 
     def __init__(self, *args, **kwargs):
         super(Table, self).__init__(*args, **kwargs)
@@ -23,39 +19,9 @@ class Table(BaseElement):
         self.HasHeader = False
         self.mapper = None
 
-    def GetDimentions(self):
-        rows, columns = 0, 0
-
-        if self.HasChild("TableHead"):
-            rows += 1
-
-        if self.HasChild("TableBody"):
-            idx, tbody = self.GetChildByName("TableBody")
-            rows += self._GetRows(tbody.GetChildren())
-            columns = self._GetColumns(tbody.GetChildren())
-        else:
-            rows += self._GetRows(self.GetChildren())
-            columns = self._GetColumns(self.GetChildren())
-
-        self.rows = rows
-        self.columns = columns
-        return rows, columns
-
-    def _GetColumns(self, tableRows):
-        max_cols = 0
-        return max(
-            [sum([cell.GetColspan() if cell.HasColspan() else 1 for cell in row.GetChildren()]) for row in tableRows]
-        )
-
-    def _GetRows(self, tableRows):
-        max_rowspan = max(
-            [max([cell.GetRowspan() if cell.HasRowspan() else 1 for cell in row.GetChildren()]) for row in tableRows]
-        )
-        return max_rowspan if max_rowspan > len(tableRows) else len(tableRows)
-
-    def _MergeCells(self, alignParagraphCenter=True):
+    def _MergeCells(self):
         """
-        merge cells in order to deal with 'colspan' attribute.
+        Merge cells in order to deal with 'colspan' and 'rowspan' attributes.
         Cells can be adjacent or not, and can span over multiple lines.
         e.g given the following table:
         -------------------
@@ -70,7 +36,7 @@ class Table(BaseElement):
          |       c11       |
          |                 |
          -------------------
-         Needs to be done here, after table has been rendered (otherwise the merged
+         This needs to be done here, after table has been rendered (otherwise the merged
          cell will have the size of the original cell), and in reversed order (from bottom/right to top/left)
          to work (after merging, the word Cell object's RowIndex and ColumnIndex properties values may change)
          """
@@ -78,9 +44,6 @@ class Table(BaseElement):
             originCell = self.Table.Cell(*originCellCoordinates)
             targetCell = self.Table.Cell(*targetCellCoordinates)
             originCell.Merge(targetCell)
-            if alignParagraphCenter:
-                originCell.Range.ParagraphFormat.Alignment = constants.wdAlignParagraphCenter
-
 
     def StartRender(self):
         self.HasHeader = self.GetChildByName("TableHead")[0] is not None
@@ -89,16 +52,16 @@ class Table(BaseElement):
         self.selection.TypeParagraph()
         self._end_range = self.selection.Range
 
-        rows, cells = self.GetDimentions()
-        self.mapper = TableMapper(self.soup, self.rows, self.columns)
+        self.mapper = TableMapper(self.soup)
+        rows, cells = self.mapper.max_rows, self.mapper.max_columns
         self.Table = self.selection.Tables.Add(rng,
                                                NumRows=rows,
                                                NumColumns=cells,
                                                AutoFitBehavior=constants.wdAutoFitFixed)
 
         for count, child in enumerate(self.GetChildren()):
-            child.row_number = count + 1
-            child.SetRow(self.Table.Rows(count+1))
+            row_number = count + 1
+            child.SetRow(self.Table.Rows(row_number), row_number)
         self.Table.Style = "Table Grid"
         self.Table.AllowAutoFit = True
 
@@ -109,26 +72,19 @@ class Table(BaseElement):
 
 
 class TableMapper(object):
-    def __init__(self, table_parsed_html, max_rows, max_columns):
-        self.table = table_parsed_html
-        self.max_rows = max_rows
-        self.max_columns = max_columns
-        self.calculateMapping()
+    def __init__(self, table_parsed_html):
+        self.calculateMapping(table_parsed_html)
 
-    def calculateMapping(self):
+    def calculateMapping(self, table_parsed_html):
         mapping = OrderedDict()
-
-        # Initialization
-        for row in range(1, self.max_rows + 1):
-            for column in range(1, self.max_columns + 1):
-                mapping[(row, column)] = (row, column)
-
-        rows = self.table.findAll('tr')
+        tablerows = table_parsed_html.findAll('tr')
 
         # Calculating offsets
-        for row_index, row in enumerate(rows, start=1):
+        for row_index, row in reversed(list(enumerate(tablerows, start=1))):
             cells = row.findAll('td')
-            for cell_index, cell in enumerate(cells, start=1):
+            for cell_index, cell in reversed(list(enumerate(cells, start=1))):
+                mapping[(row_index, cell_index)] = (row_index, cell_index)
+
                 rowspan = int(cell.get('rowspan', 1))
                 colspan = int(cell.get('colspan', 1))
                 #adjusts rowspan
@@ -151,9 +107,13 @@ class TableMapper(object):
                         row, column = mapping[key]
                         mapping[key] = row, column + colspan - 1
 
+        # Calculate the maximum table size
+        rows, columns = zip(*mapping.values())
+        self.max_rows, self.max_columns = max(rows), max(columns)
+
         # Calculating cells to merge
         cells_to_merge = []
-        for row_index, row in enumerate(rows, start=1):
+        for row_index, row in enumerate(tablerows, start=1):
             cells = row.findAll('td')
             for cell_index, cell in enumerate(cells, start=1):
                 new_row_index, new_column_index = mapping[(row_index, cell_index)]
@@ -176,32 +136,26 @@ class TableBody(IgnoredElement):
 
 class TableHead(BaseElement):
     def SetRow(self, Row):
-        self.GetChildren()[0].SetRow(Row)
+        self.GetChildren()[0].SetRow(Row, 1)
 
 
 class TableRow(BaseElement):
     AllowedChildren = ["TableCell"]
-    row_number = None
 
     def __init__(self, *args, **kwargs):
         super(TableRow, self).__init__(*args, **kwargs)
         self.Row = None
         self.IsHeader = False
 
-    def GetChildren(self):
-        children = super(TableRow, self).GetChildren()
-        map((lambda x: x.SetParent(self)), children)
-        return children
-
-    def SetRow(self, Row):
+    def SetRow(self, Row, row_number):
         self.Row = Row
+        self.row_number = row_number
 
     def StartRender(self):
         self.IsHeader = self.GetParent().GetName == "TableHead"
         mapper = self.GetParent().mapper
         for count, child in enumerate(self.GetChildren()):
             assert child.GetName() == "TableCell", "Child of TableRow is not TableCell! Its %s" % child.GetName()
-            child.SetParent(self)
             new_mapping = mapper.mapping[(self.row_number, count+1)]
             child.SetCell(self.Row.Cells(new_mapping[1]))
 
@@ -215,36 +169,4 @@ class TableCell(BaseElement):
 
     def StartRender(self):
         self.Cell.Range.Select()
-    
-    def HasColspan(self):
-        return self._HasSpanAttribute('colspan')
-    
-    def GetColspan(self):
-        return self._GetSpanAttribute('colspan')
-
-    def HasRowspan(self):
-        return self._HasSpanAttribute('rowspan')
-
-    def GetRowspan(self):
-        return self._GetSpanAttribute('rowspan')
-
-    def _GetSpanAttribute(self, attribute_name):
-        if attribute_name not in ('colspan', 'rowspan'):
-            return None
-        return int(self.GetAttrs().get(attribute_name))
-
-    def _HasSpanAttribute(self, attribute_name):
-        attr = self.GetAttrs().get(attribute_name)
-        if attr:
-            try:
-                int_attr = int(attr)
-                if int_attr > 1:
-                    return True
-            except ValueError:
-                warnings.warn("'%s' is not a valid value for %s attribute" % (attr, attribute_name))
-                return False
-        return False
-
-
-
 
